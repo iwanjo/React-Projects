33747825829c19df5687e897959bf3dca228238b
Ian <kamanuian@gmail.com>
2020-04-13 15:01:22 +0300

    Budgeting React Project

---
+++ budgeting_app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <div className = "header">
+            <h1>The Budgeting App</h1>
+            
+        </div>
+    )
+}
+
+export default header
